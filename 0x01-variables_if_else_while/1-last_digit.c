ec71f499fdae587ba8527a881f1d606193c44aa2
Ifeanyi <ifeanyi42730@gmail.com>
2022-10-15 15:54:40 -0700

    removed whitespaces

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+ * main - assign a random number
+ * Return: (0) successful
+ */
+int main(void)
+{
+	int n;
+	int a;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	a = n % 10;
+
+	if (a > 5)
+	{
+		printf("Last digit of %d and %d is greater than 5\n", n, a);
+	}
+	else if (a == 0)
+	{
+		printf("Last digit of %d and %d is 0\n", n, a);
+	}
+	else
+	{
+		printf("Last digit of %d and %d is less than 6 and not 0\n", n, a);
+	}
+	return (0);
+}
