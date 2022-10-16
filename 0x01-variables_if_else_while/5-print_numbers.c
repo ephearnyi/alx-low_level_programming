934364a85d4263bd2d3c0a4d8e8e7d9921f6ad23
Ifeanyi <ifeanyi42730@gmail.com>
2022-10-16 00:13:03 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers in base 10
+ * Return: (0) successful
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		printf("%d", num);
+	}
+	printf('\n');
+	return (0);
+}
