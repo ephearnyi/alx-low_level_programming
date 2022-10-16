da37f78f38e602e034447fe421739c86e1588cff
Ifeanyi <ifeanyi42730@gmail.com>
2022-10-16 05:13:28 -0700

    com3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,16 +10,17 @@ int main(void)
 
 	for (num1 = 0; num1 <= 9; num1++)
 	{
-		putchar(num1 + '0');
-	}
-	for (num2 = num1 + 1; num2 <= 9; num2++)
-	{
-		putchar(num2 + '0');
-	}
-	if (num1 < 8)
-	{
-		putchar(',');
-		putchar(' ');
+		for (num2 = num1 + 1; num2 <= 9; num2++)
+		{
+			putchar(num1 + '0');
+			putchar(num2 + '0');
+
+			if (num1 < 8)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
 	}
 	putchar('\n');
 	return (0);
