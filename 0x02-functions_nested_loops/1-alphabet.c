708047f4c772ded298a4496600ab772b9221152e
Ifeanyi <ifeanyi42730@gmail.com>
2022-10-19 01:10:13 -0700

    Alpha. removed return

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,7 +2,6 @@
 
 /**
  * print_alphabet - prints alphabet in lowercase
- * Return: Always 0
  */
 void print_alphabet(void)
 {
@@ -13,5 +12,4 @@ void print_alphabet(void)
 		_putchar(alpha);
 	}
 	_putchar('\n');
-	return (0);
 }
