dbdebd2edca9823e80c0a5c5e9f73a55852cfd66
Ifeanyi <ifeanyi42730@gmail.com>
2022-10-19 08:55:30 -0700

    signs

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sign - print the sign befor numbers
+ * @n: number declaration
+ *
+ * Return: 1 if n is greater than zero, 0 if > zero -1 if otherwise
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+}
