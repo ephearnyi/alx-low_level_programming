1a9d31438263a093a2fa47e140ffbf2ee4e42af0
Ifeanyi <ifeanyi42730@gmail.com>
2022-10-19 21:27:36 -0700

    fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - print fibonacci series
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+	int count;
+	unsigned long fib1 = 0, fib2 = 1, sum;
+
+	for (count = 0; count < 50; count++)
+	{
+		sum = fib1 + fib2;
+		printf("%lu", sum);
+
+		fib1 = fib2;
+		fib2 = sum;
+
+		if (count == 49)
+			printf("\n");
+		else
+			printf("\n");
+	}
+	return (0);
+}
