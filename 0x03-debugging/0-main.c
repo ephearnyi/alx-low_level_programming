8513131b2d128e37a0a923a21270104865bbc7c1
Ifeanyi <ifeanyi42730@gmail.com>
2022-10-20 07:15:06 -0700

    zero task

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - check positive or negative numbers
+ * Return: 0 always
+ */
+int main(void)
+{
+	int n;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
