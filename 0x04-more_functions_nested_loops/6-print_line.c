4cbd67c26c0345855ca9b432d1b937110a2f0307
Ifeanyi <ifeanyi42730@gmail.com>
2022-10-21 07:34:31 -0700

    Corrected number 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,12 +9,11 @@ void print_line(int n)
 {
 	int i;
 
-	for (i = 1; i < n; n++)
+	if (n <= 0)
+		_putchar('\n');
+	for (i = 1; i < n; i++)
 	{
-		if (n > 0)
-			_putchar('_');
-		else
-			_putchar('\n');
+		_putchar('_');
 	}
 	_putchar('\n');
 }
