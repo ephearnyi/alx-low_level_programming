ff1230d34c700d67591d0837e40d11d1897fd71f
Ifeanyi <ifeanyi42730@gmail.com>
2022-10-21 02:45:49 -0700

    task 0 correct

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -4,10 +4,10 @@
  * _isdigit - write function that checks digits
  * @c: char to check
  * Return: 0 or 1
- */
+ **/
 int _isdigit(int c)
 {
-	if (c >= 0 && c <= 9)
+	if (c >= '0' && c <= '9')
 		return (1);
 	else
 		return (0);
