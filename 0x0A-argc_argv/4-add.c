c9c11439b4bd69b0c790034a75cbac43e7175a01
Ifeanyi <ifeanyi42730@gmail.com>
2022-11-04 15:51:35 -0700

    task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,34 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+/**
+ * check_num - check string for digits
+ * @str: string array
+ * Return: 0
+ */
+int check_num(char *str)
+{
+	unsigned int i;
+
+	i = 0;
+	while (i < strlen(str))
+	{
+		if (!isdigit(str[i]))
+		{
+			return (0);
+		}
+		i++;
+	}
+	return (0);
+}
 /**
  * main - add positive num
  * @argc: args count
  * @argv: args variable
  * Return: 0
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	int i, n, sum = 0;
-	char *flag;
+	int i;
+	int str_to_int;
+	int sum = 0;
 
-	if (argc < 2)
+	i = 1;
+	while (i < argc)
 	{
-		printf("0\n");
-		return (0);
-	}
-
-	for (i = 1; argv[i]; i++)
-	{
-		n = strtol(argv[i], &flag, 10);
-		if (*flag)
+		if (check_num(argv[i]))
+		{
+			str_to_int = atoi(argv[i]);
+			sum += str_to_int;
+		}
+		else
 		{
 			printf("Error\n");
-			return (1);
+			return (0);
 		}
-	}
-	else
-	{
-		sum += n;
+		i++;
 	}
 	printf("%d\n", sum);
 	return (0);
