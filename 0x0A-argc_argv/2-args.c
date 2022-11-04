e69e9505797ce99229cac8397612b60c695f0aca
Ifeanyi <ifeanyi42730@gmail.com>
2022-11-04 15:21:05 -0700

    task 2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -7,12 +7,15 @@
  */
 int main(int argc, char **argv)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0, i < argc, i++)
+	if (argc > 0)
 	{
-		printf("%s\n", argv[i]);
-		i++;
+		while (i < argc)
+		{
+			printf("%s\n", argv[i]);
+			i++;
+		}
 	}
 	return (0);
 }
