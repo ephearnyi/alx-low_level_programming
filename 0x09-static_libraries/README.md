889cb67d68411959f681cda211ccfb5c231a2d08
Ifeanyi <ifeanyi42730@gmail.com>
2022-11-04 17:33:04 -0700

    tasks

---
+++ 0x09-static_libraries/README.md
@@ -0,0 +1,1 @@
+This contains assignment for static library
---
+++ 0x09-static_libraries/_putchar.c
@@ -0,0 +1,10 @@
+#include <unistd.h>
+/**
+ * _putchar - writes the character c to stdout
+ * @c: the char to print
+ * Return: 1 success
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
