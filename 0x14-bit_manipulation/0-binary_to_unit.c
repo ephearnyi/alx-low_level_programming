bba011eef967745b6d278783af1bc4ae55352c10
Ifeanyi <ifeanyi42730@gmail.com>
2022-12-01 11:01:29 -0800

    bit manipulation

---
+++ 0x14-bit_manipulation/0-binary_to_unit.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * binary_to_unit - Converts a binary to an unsigned int
+ * @b: pointer to the character
+ * Return: The converted number, 0 if extra characters or b = NULL
+ */
+unsigned int binary_to_unit(const char *b)
+{
+	int i;
+	unsigned int j;
+
+	j = 0;
+	if (!b)
+		return (0);
+
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+	}
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		j <<= 1;
+		if (b[i] == '1')
+			j += 1;
+	}
+	return (j);
+}
